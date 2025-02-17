501abfbe84209349891892b894ec2db6d3ae94d0
silvianisaaulia <143465936+silvianisaaulia@users.noreply.github.com>
2025-02-17 12:37:34 +0700

    pertama

---
+++ register.php
@@ -0,0 +1,98 @@
+<?php
+include "koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Register Ke ToDoList</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-secondary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header bg-dark" style="color: white;"><h3 class="text-center font-weight-light my-4">Register Pepustakaan Digital</h3></div>
+                                    <div class="card-body" style="background-color:gainsboro">
+                                        <?php
+                                        if(isset($_POST['register'])) {
+                                            $nama = $_POST['nama'];
+                                            $email = $_POST['email'];
+                                            $alamat = $_POST['alamat'];
+                                            $no_telepon = $_POST['no_telepon'];
+                                            $username = $_POST['username'];
+                                            $level = $_POST['level'];
+                                            $password = md5($_POST['password']); 
+                                            
+                                            $insert = mysqli_query($koneksi ,"INSERT INTO user(nama,email,alamat,no_telepon,username,password,level) VALUES('$nama','$email','$alamat','$no_telepon','$username','$password','$level')");
+
+                                            if($insert){
+                                                echo '<script>alert("Selamat, Register Berhasil. Silahkan Login."); location.href="login.php"</script>';
+                                            }else{
+                                                echo '<script>alert("Register Gagal, Silahkan Ulangi Kembali");</script>';
+                                            }
+                                        }
+                                        ?>
+                                        <form method="post">
+                                            <div class="form-floating mb-5 ">
+                                                <input class="form-control" type="text" required name="nama" placeholder="Masukkan Nama Lengkap" />
+                                                <label >Nama Lengkap</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <input class="form-control" type="username" required name="email" placeholder="Masukkan Username" />
+                                                <label >Email</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <input class="form-control" type="username" required name="no_telepon" placeholder="Masukkan No. Telepon" />
+                                                <label >No. Telepon</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <textarea name="alamat" rows="5" required class="form-control"></textarea>
+                                                <label >Alamat</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <textarea name="username" rows="5" required class="form-control"></textarea>
+                                                <label >Username</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <input class="form-control" id="inputPassword" required name="password" type="password" placeholder="Masukkan Password" />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="form-floating mb-5">
+                                                <select name="level" required class="form-select form-control ">
+                                                    <option value="Peminjam">Peminjam</option>
+                                                    <option value="admin">admin</option>
+                                                </select>
+                                                <label >Level</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <button class="btn btn-primary" type="submit" name="register" value="register">Register</button>
+                                                <a class="btn btn-danger" href="login.php">Login</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3 bg-dark">
+                                        <div class="small"><a href="register.html">
+                                            &copy; 2025 ToDoList.
+                                        </a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
